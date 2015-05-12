cac4f3bbdddb66720fc7250d3aac93b834d41c26
TsimafeiKhatkevich <tsimafej@gmail.com>
2015-05-12 15:26:11 +0300

    Some refactoring + added model generator class sceleton

--- common_lib.h
+++ common_lib.h
@@ -1,23 +1,9 @@
 #pragma once
 
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <assert.h>
+#include <map>
 #include <unordered_map>
-
-// accepted params: start_size, steps, out_deg_half, alpha, beta, delta
-struct TModelConfig {
-    uint32_t NStartVertices;
-    uint32_t NSteps;
-    uint32_t OutDegree;
-
-    double Alpha;
-    double Beta;
-    double Delta;
-
-    void ParseFromFile(const std::string& fileName);
-};
+#include <random>
 
 namespace NMath {
 
@@ -36,16 +22,15 @@ namespace NMath {
 
     const uint32_t BIG_PRIME = 1000000007;
 
+    typedef std::pair<uint32_t, uint32_t> TIntPair;
+
+    struct TIntPairHasher {
+        size_t operator ()(const TIntPair& pair) const {
+            return pair.first + BIG_PRIME * pair.second;
+        }
+    };
+
     class TCachedBinomialCalcer {
-    public:
-        typedef std::pair<uint32_t, uint32_t> TIntPair;
-
-        struct TIntPairHasher {
-            size_t operator ()(const TIntPair& pair) const {
-                return pair.first + BIG_PRIME * pair.second;
-            }
-        };
-
     public:
         TCachedBinomialCalcer(size_t maxSize = 1000000)
             : MaxSize(maxSize)
@@ -53,28 +38,45 @@ namespace NMath {
             Storage.reserve(MaxSize);
         }
 
-    uint64_t Calc(uint32_t n, uint32_t k) {
-        if (!k || n <= k) {
-            return 1;
-        }
+        uint64_t Calc(uint32_t n, uint32_t k) {
+            if (!k || n <= k) {
+                return 1;
+            }
 
-        const auto it = Storage.find(TIntPair(n, k));
-        if (it != Storage.end()) {
-            return it->second;
-        }
+            const auto it = Storage.find(TIntPair(n, k));
+            if (it != Storage.end()) {
+                return it->second;
+            }
 
-        auto result = Calc(n - 1, k - 1) + Calc(n - 1, k);
-        if (Storage.size() >= MaxSize) {
-            Storage.erase(Storage.begin());
+            auto result = Calc(n - 1, k - 1) + Calc(n - 1, k);
+            if (Storage.size() >= MaxSize) {
+                Storage.erase(Storage.begin());
+            }
+            Storage[TIntPair(n, k)] = result;
+            return result;
         }
-        Storage[TIntPair(n, k)] = result;
-        return result;
-    }
 
     private:
         size_t MaxSize;
         std::unordered_map<TIntPair, uint64_t, TIntPairHasher> Storage;
     };
+
+    template<class TVal>
+    inline TVal SampleValueFromCDF(const std::map<double, TVal>& cdf) {
+        static std::random_device generator;
+        static std::uniform_real_distribution<double> dist(0., 1.);
+        dist.reset();
+
+        const double randReal = dist(generator);
+        return cdf.lower_bound(randReal);
+    }
+
+    inline size_t GetRandomIndex(size_t max) {
+        static std::random_device generator;
+        static std::uniform_int_distribution<size_t> dist(0, max);
+        dist.reset();
+        return dist(generator);
+    }
 
 } // NMath
 
